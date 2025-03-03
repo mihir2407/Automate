df6b87c0aa5785b65a10dcd6b9ce012d20edca47
mihir2407 <mihir.patel6@ibm.com>
2025-03-03 12:23:41 -0500

    https://github.com/mihir2407/Automate.git

---
+++ cx-as-code-cicd-gitactions-blueprint-main/cx-as-code-cicd-gitactions-blueprint-main/blueprint/genesys-cloud-cx-as-code/main.tf
@@ -0,0 +1,43 @@
+
+
+terraform {
+
+  backend "remote" {
+    organization = "thoughtmechanix"
+
+    workspaces {
+      prefix = "genesys_email_"
+    }
+  }
+
+  required_providers {
+    genesyscloud = {
+      source = "mypurecloud/genesyscloud"
+    }
+  }
+}
+
+provider "genesyscloud" {
+  sdk_debug = true
+}
+
+module "classifier_users" {
+  source = "./modules/users"
+}
+
+module "classifier_queues" {
+  source                   = "./modules/queues"
+  classifier_queue_names   = ["401K", "IRA", "ROTH", "529","GeneralSupport"]
+  classifier_queue_members = module.classifier_users.user_ids
+}
+
+module "classifier_email_routes" {
+  source               = "./modules/email_routes"
+  genesys_email_domain = var.genesys_email_domain
+}
+
+module "classifier_data_actions" {
+  source             = "./modules/data_actions"
+  classifier_url     = var.classifier_url
+  classifier_api_key = var.classifier_api_key
+}
